2ff036791367bb96c5606ef5c6e6e97f5ff6e3a3
Ludwing-X <157496923+Ludwing-X@users.noreply.github.com>
2024-08-09 18:05:38 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+import telebot
+from telebot.types import ReactionTypeEmoji
+
+load_dotenv()
+bot_token = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(bot_token)
+
+if "BOT_TOKEN" in os.environ:
+    print("Check")
+else:
+    print("Not check")
+
+@bot.message_handler()
+def clown_react(message):
+    if message.from_user.id == 5566979429:    # To debug, substitute with any id
+        print("React")
+        bot.set_message_reaction(message.chat.id, message.id, [ReactionTypeEmoji("🤡")])
+    else:
+        # print(message.from_user.id)   # Debug snippet
+        # print("Not react")
+        pass
+
+bot.infinity_polling()
